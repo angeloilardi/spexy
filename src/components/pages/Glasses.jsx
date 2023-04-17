dfce41b41214ba12a453f91673308259fb8fa17e
Angelo Ilardi <ange448@gmail.com>
2023-04-17 16:50:22 +0100

    added show on scroll effect

--- src/components/pages/Glasses.jsx
+++ src/components/pages/Glasses.jsx
@@ -3,20 +3,20 @@ import glasses from './../../data/data.json'
 
 export default function Glasses() {
   return (
-    <div className="container pt-5">
+    <div className="container-fluid pt-5 ">
       <div className="row">
         {glasses.map((card) => {
           return (
             <div className="col-lg-4" key={card.id}>
-              <div className="border">
+              <div className="border position-relative p-1">
                 <Fade delay={300}>
                   <img
-                    className="img-fluid border-black card-img opacity-75"
+                    className="img-fluid border-black card-img"
                     src={card.url}
                     alt=""
                   />
-                  <div className="position-relative top-0 start-0">
-                    <h2 className=" text-dark">FIND THE PERFECT FIT</h2>
+                  <div className="position-absolute bottom-0 start-0 p-3">
+                    <h2 className=" text-black bg-light bg-gradient">{ card.caption.toUpperCase()}</h2>
                   </div>
                 </Fade>
               </div>
