e4e083c6db6f4c1ce38cc674760c0152973e997b
Angelo Ilardi <ange448@gmail.com>
2023-05-18 21:23:11 +0100

    possible fix for home vh

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="position-relative home">
           <img
             src="https://images.pexels.com/photos/837306/pexels-photo-837306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            style={{ objectFit: "cover", width: "100%", height: "94vh" }}
+            style={{ objectFit: "cover", width: "100%", height: "100vh" }}
             alt="man with sunglasses"
           />
           <div className="position-absolute bottom-0 start-0 pb-5 ps-2">
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="position-relative home">
           <img
             src="https://images.pexels.com/photos/1627639/pexels-photo-1627639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            style={{ objectFit: "cover", width: "100%", height: "94vh" }}
+            style={{ objectFit: "cover", width: "100%", height: "100vh" }}
             alt="glasses in a drawer"
           />
           <div className="position-absolute bottom-0 start-0 pb-5 ps-2">
