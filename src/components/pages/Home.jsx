b67c44389d07162fef9d5b4080e59b8995581d20
Angelo Ilardi <ange448@gmail.com>
2023-06-07 08:27:41 +0100

    uncompressed images

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="d-flex flex-row wv-100 pt-5">
       <div className="position-relative home">
         <img
-          src="https://images.pexels.com/photos/837306/pexels-photo-837306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/837306/pexels-photo-837306.jpeg"
           style={{ objectFit: "cover", width: "100%", height: "100svh" }}
           alt="man with sunglasses"
         />
@@ -21,7 +21,7 @@ export default function Home() {
 
       <div className="position-relative home">
         <img
-          src="https://images.pexels.com/photos/1627639/pexels-photo-1627639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/1627639/pexels-photo-1627639.jpeg"
           style={{ objectFit: "cover", width: "100%", height: "100svh" }}
           alt="glasses in a drawer"
         />
